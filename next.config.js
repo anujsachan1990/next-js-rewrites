48397dcf54e97352ba4a1cf9434d0a85585a8791
Anuj Sachan <anuj.sachan@akqa.com>
2022-08-22 17:03:43 +1000

    added simple rewrite

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+const rewrites = () => {
+  return [
+    {
+      source: '/',
+      destination: 'https://www.countryroad.com.au/',
+      permanent: true,
+    },
+  ]
+}
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  rewrites
 }
+
 
 module.exports = nextConfig
