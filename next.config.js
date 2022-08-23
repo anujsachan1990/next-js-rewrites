ccc5032474355dc683c546883f381ec38bc39fc4
Anuj Sachan <anuj.sachan@akqa.com>
2022-08-23 11:39:17 +1000

    test fallback

--- next.config.js
+++ next.config.js
@@ -1,16 +1,26 @@
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/:slug*/',
+  //       destination: 'https://www.countryroad.com.au/:slug*/',
+  //     },
+  //     {
+  //       source: '/:slug*',
+  //       destination: 'https://www.countryroad.com.au/:slug*',
+  //     }
+  //   ]
+  // },
   async rewrites() {
-    return [
-      {
-        source: '/:slug*/',
-        destination: 'https://www.countryroad.com.au/:slug*/',
-      },
-      {
-        source: '/:slug*',
-        destination: 'https://www.countryroad.com.au/:slug*',
-      }
-    ]
+    return {
+      fallback: [
+        {
+          source: '/:path*',
+          destination: `https://www.countryroad.com.au/:path*`,
+        },
+      ],
+    }
   },
 }
