c464e59c7aa7b23864ca9ffccc81fd48aba0f503
Anuj Sachan <anuj.sachan@akqa.com>
2022-08-23 11:25:19 +1000

    tets

--- next.config.js
+++ next.config.js
@@ -1,24 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
-  async redirects() {
-    return [
-      {
-        source: '/about',
-        destination: '/',
-        permanent: true,
-      },
-    ]
-  },
   async rewrites() {
     return [
       {
         source: '/:slug*/',
         destination: 'https://www.countryroad.com.au/:slug*/',
-      },
-      {
-        source: '/:path*/',
-        destination: 'https://www.countryroad.com.au/:path*/',
       }
     ]
   },
