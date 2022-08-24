4a5bd32b7187a809f07d76794e84ac4cf74ddd13
Anuj Sachan <anuj.sachan@akqa.com>
2022-08-24 18:31:51 +1000

    test

--- next.config.js
+++ next.config.js
@@ -16,6 +16,12 @@ module.exports = {
   // //     }
   // //   ]
   // // },
+  headers: [
+    {
+      key: 'cookie',
+      value: 'visid_incap_2179657=TQkQqgNoS2S4aAxVWqpp/JL0AmMAAAAAQUIPAAAAAADYKSD82ucL5fJzGLRJE1d9; incap_ses_362_2179657=L4v9FvrejkqzvzuDMIwGBXwSBGMAAAAAGNsxAzPeyqXXYU87da4A4w==; incap_ses_972_2179657=zvxmAKJ2jGYzJxBxgT59Ded1BWMAAAAABBAD+LYQ7zj8c4fd4tapOg==; incap_ses_971_2179657=9CtTN5+E0wvYpxlvALF5DS7TBWMAAAAAY+0TR82G9escy5lAhGIdjg==;',
+    },
+  }
   async rewrites() {
  
     return {
