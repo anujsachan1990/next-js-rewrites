6b7466435b70c94cf25986148a6007b19e01dcd2
Anuj Sachan <anuj.sachan@akqa.com>
2022-08-24 18:48:44 +1000

    test

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   const x = fetch('https://www.countryroad.com.au', {
     method: req.method,
     redirect: 'manual',
-    credentials: 'same-origin'
+    credentials: 'include'
   })
 
   return x
