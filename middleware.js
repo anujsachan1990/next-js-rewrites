3792e6929a84f672f912c2a0e63ed0c9dd34552b
Anuj Sachan <anuj.sachan@akqa.com>
2022-08-24 18:54:49 +1000

    test

--- middleware.js
+++ middleware.js
@@ -9,7 +9,7 @@ export async function middleware(req) {
 
   // Rewrite to the correct url
 
-  const url = new URL(req.nextUrl.pathname, 'https://www.countryroad.com.au')
+  const url = new URL(req.nextUrl.pathname, 'https://www.countryroad.com.au/')
   console.log("test---->", url.href)
   // Return rewrited path
   return NextResponse.rewrite(url)
