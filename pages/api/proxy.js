89faf58765211d0ec20f8f509beadcb42530044c
Anuj Sachan <anuj.sachan@akqa.com>
2022-08-24 20:59:32 +1000

    test

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -3,15 +3,11 @@ export const config = {
 }
 
 export default async function handler(req, res) {
-  console.log('proxy is here');
-  const x = fetch('https://golf.org.au/', {
-    method: req.method,
-    redirect: 'manual',
-    headers: {
-      Cookie: 'visid_incap_2179657=TQkQqgNoS2S4aAxVWqpp/JL0AmMAAAAAQUIPAAAAAADYKSD82ucL5fJzGLRJE1d9; incap_ses_362_2179657=L4v9FvrejkqzvzuDMIwGBXwSBGMAAAAAGNsxAzPeyqXXYU87da4A4w==; incap_ses_972_2179657=zvxmAKJ2jGYzJxBxgT59Ded1BWMAAAAABBAD+LYQ7zj8c4fd4tapOg==; incap_ses_971_2179657=9CtTN5+E0wvYpxlvALF5DS7TBWMAAAAAY+0TR82G9escy5lAhGIdjg==;'
-    }
-  })
 
-  return x
+  const url = new URL(req.url, 'https://www.countryroad.com.au/')
+  console.log("test---->", url.href)
+  // Return rewrited path
+  return res.rewrite(url) // NextResponse.rewrite(url)
+  
 
 }
