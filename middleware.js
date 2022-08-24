94f39d6b354627200a852d9b86febdf92598fa51
Anuj Sachan <anuj.sachan@akqa.com>
2022-08-24 16:00:46 +1000

    added experimental edge function

--- middleware.js
+++ middleware.js
@@ -6,8 +6,8 @@ export async function middleware(req) {
  
   // Rewrite to the correct url
 
-  const url = new URL(req.nextUrl.pathname, 'https://www.countryroad.com.au')
-  console.log("test---->", url.href)
-  // Return rewrited path
-  return NextResponse.rewrite(url)
+  // const url = new URL(req.nextUrl.pathname, 'https://www.countryroad.com.au')
+  // console.log("test---->", url.href)
+  // // Return rewrited path
+  // return NextResponse.rewrite(url)
 }
