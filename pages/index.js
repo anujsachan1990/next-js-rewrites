8f18a4b9d2d15872c82d293aa5e35b1e72394fa3
Anuj Sachan <anuj.sachan@akqa.com>
2022-08-24 15:52:46 +1000

    added experimental edge function

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home({ html }) {
 export async function getServerSideProps(context) {
   const { req } = context;
 
-  const x = await fetch('http://localhost:3000/api/proxy/')
+  const x = await fetch('https://next-js-rewrites-anuj.vercel.app/api/proxy/')
   const html = await x.text()
   console.log('response from proxy')
   return {
