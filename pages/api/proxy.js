6320800571c5fe17e000b0ec941411370af3e1eb
Anuj Sachan <anuj.sachan@akqa.com>
2022-08-24 21:01:37 +1000

    test

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -2,12 +2,8 @@ export const config = {
   runtime: 'experimental-edge',
 }
 
-export default async function handler(req, res) {
-
+export default async (req, res) => {
   const url = new URL(req.url, 'https://www.countryroad.com.au/')
   console.log("test---->", url.href)
-  // Return rewrited path
-  return res.rewrite(url) // NextResponse.rewrite(url)
-  
-
+  return res.rewrite(url)
 }
