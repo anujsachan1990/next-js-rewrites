c4b0a53b9114322d8d3d1c40a53dc28b202ce869
Anuj Sachan <anuj.sachan@akqa.com>
2022-08-24 19:04:15 +1000

    qdqw

--- middleware.js
+++ middleware.js
@@ -5,10 +5,10 @@ import { NextResponse } from 'next/server'
 export async function middleware(req) {
 
 
-  // Rewrite to the correct url
+  // // Rewrite to the correct url
 
-  const url = new URL(req.nextUrl.pathname, 'https://www.countryroad.com.au/')
-  console.log("test---->", url.href)
-  // Return rewrited path
-  return NextResponse.rewrite(url)
+  // const url = new URL(req.nextUrl.pathname, 'https://www.countryroad.com.au/')
+  // console.log("test---->", url.href)
+  // // Return rewrited path
+  // return NextResponse.rewrite(url)
 }
