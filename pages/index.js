25b960c67f230018b97cd6c02577c7adcbc2ca8d
Anuj Sachan <anuj.sachan@akqa.com>
2022-08-25 10:13:46 +1000

    change

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,7 @@ export default function Home({ html }) {
 
 export async function getServerSideProps(context) {
  
-  const x = await fetch('https://next-js-rewrites-anuj.vercel.app/api/proxy/',{
-    credentials: "same-origin"
-  })
+  const x = await fetch('https://next-js-rewrites-anuj.vercel.app/api/proxy/')
 
   const html = await x.text()
   console.log('response from proxy', html)
