0f9f0e0527a6182d8499a351c98cbf9143e174b8
Anuj Sachan <anuj.sachan@akqa.com>
2022-08-26 16:59:31 +1000

    test

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -5,15 +5,19 @@ export const config = {
 export default async function handler(req) {
 
   console.log('cookies', req.cookies)
-  
+
   const response = await fetch(`${process.env.REWRITE_HOST}${req.nextUrl.pathname}`, {
     method: req.method,
+    credentials: "same-origin",
     redirect: 'manual',
   })
-  if (!['html', 'css', 'javascript'].some((type) => response.headers?.get('content-type').includes(type))) {
-    return response
+  if (!!response.headers.get('content-type')) {
+    if (!['html', 'css', 'javascript'].some((type) => response.headers.get('content-type').includes(type))) {
+      return response
+    }
   }
-  
+
+
   const text = await response.text()
   const modifiedtext = text.replaceAll(process.env.REWRITE_HOST, req.nextUrl.origin)
   const { headers } = response
