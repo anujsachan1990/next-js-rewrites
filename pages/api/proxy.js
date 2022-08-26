59c91765f90065def5bd5a6ab98b626eca04054f
Anuj Sachan <anuj.sachan@akqa.com>
2022-08-26 16:53:27 +1000

    test

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -10,9 +10,10 @@ export default async function handler(req) {
     method: req.method,
     redirect: 'manual',
   })
-  if (!['html', 'css', 'javascript'].some((type) => response.headers.get('content-type').includes(type))) {
+  if (!['html', 'css', 'javascript'].some((type) => response.headers?.get('content-type').includes(type))) {
     return response
   }
+  
   const text = await response.text()
   const modifiedtext = text.replaceAll(process.env.REWRITE_HOST, req.nextUrl.origin)
   const { headers } = response
