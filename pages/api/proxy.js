e1369952175815eb9d5fd53b47e2436c6123bdcc
Anuj Sachan <anuj.sachan@akqa.com>
2022-08-30 12:40:21 +1000

    qdqdqw

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -4,35 +4,38 @@ export const config = {
 }
 
 export default async function handler(req) {
-  let responseHeader = '';
-  let requestHeader = '';
+  let responseHeader = [];
+  let requestHeader = [];
 
   console.log('cookies', req.cookies)
   req.headers.forEach((value, key) => {
-    requestHeader += `${key}: ${value}\n`
+    requestHeader.push([`${key}: ${value.replaceAll('.vercel.app', '.countryroad.com.au').replaceAll('next-js-rewrites-anuj.vercel.app', 'www.countryroad.com.au')}`])
   })
   console.log('requestHeader', requestHeader)
-  
-  requestHeader.replaceAll( '.vercel.app', '.countryroad.com.au')
-  requestHeader.replaceAll('next-js-rewrites-anuj.vercel.app', 'www.countryroad.com.au' )
-  
+
+  // requestHeader.replaceAll( '.vercel.app', '.countryroad.com.au')
+  // requestHeader.replaceAll('next-js-rewrites-anuj.vercel.app', 'www.countryroad.com.au' )
+
   console.log('requestHeaderModified', requestHeader)
+  const newRequestHeaders = new Headers(requestHeader)
+
 
   const response = await fetch(`${process.env.REWRITE_HOST}${req.nextUrl.pathname}`, {
     method: req.method,
+    headers: newRequestHeaders
   })
   console.log("response header--->")
 
   response.headers.forEach((value, key) => {
-    responseHeader += `${key}: ${value}\n`
+    responseHeader += [`${key}: ${value.replaceAll('.countryroad.com.au', '.vercel.app').replaceAll('www.countryroad.com.au', 'next-js-rewrites-anuj.vercel.app')}`]
   })
 
   console.log('responseHeader', responseHeader)
-  
 
-  responseHeader.replaceAll('.countryroad.com.au', '.vercel.app')
-  responseHeader.replaceAll('www.countryroad.com.au', 'next-js-rewrites-anuj.vercel.app')
-  
+
+  // responseHeader.replaceAll('.countryroad.com.au', '.vercel.app')
+  // responseHeader.replaceAll('www.countryroad.com.au', 'next-js-rewrites-anuj.vercel.app')
+
   console.log('responseHeaderModified', responseHeader)
 
   if (!!response.headers.get('content-type') && !['html', 'css', 'javascript'].some((type) => response.headers.get('content-type').includes(type))) {
