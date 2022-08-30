56e512a729abcdb5bae3be51d2c77cc49192f11b
Anuj Sachan <anuj.sachan@akqa.com>
2022-08-30 10:51:31 +1000

    qwdqwdwq

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -4,18 +4,26 @@ export const config = {
 }
 
 export default async function handler(req) {
+  let responseHeader = '';
+  let requestHeader = '';
 
   console.log('cookies', req.cookies)
+  req.headers.forEach((value, key) => {
+    requestHeader += `${key}: ${value}\n`
+  })
+  console.log('requestHeader', requestHeader)
 
   const response = await fetch(`${process.env.REWRITE_HOST}${req.nextUrl.pathname}`, {
     method: req.method,
 
   })
   console.log("response header--->")
-  
+
   response.headers.forEach((value, key) => {
-    console.log(key, ':', value)
+    responseHeader += `${key}: ${value}\n`
   })
+
+  console.log('responseHeader', responseHeader)
 
 
   if (!!response.headers.get('content-type') && !['html', 'css', 'javascript'].some((type) => response.headers.get('content-type').includes(type))) {
