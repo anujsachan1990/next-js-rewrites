d240cd396dbc2ef673b4f4928f7aa5c34701296c
Anuj Sachan <anuj.sachan@akqa.com>
2022-08-30 12:56:16 +1000

    qdqdqdq

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -4,39 +4,34 @@ export const config = {
 }
 
 export default async function handler(req) {
-  let responseHeader = [];
-  let requestHeader = [];
+  let responseHeader = '';
+  let requestHeader = '';
 
   console.log('cookies', req.cookies)
   req.headers.forEach((value, key) => {
-    requestHeader.push([key, `${value.replaceAll('.vercel.app', '.countryroad.com.au').replaceAll('next-js-rewrites-anuj.vercel.app', 'www.countryroad.com.au')}`])
+    requestHeader += `${key}: ${value}\n`
   })
   console.log('requestHeader', requestHeader)
-
-  // requestHeader.replaceAll( '.vercel.app', '.countryroad.com.au')
-  // requestHeader.replaceAll('next-js-rewrites-anuj.vercel.app', 'www.countryroad.com.au' )
-
-  console.log('requestHeaderModified', requestHeader)
-  const newRequestHeaders = new Headers(requestHeader)
-
+  
+  req.headers.set('set-cookie', 'anuj is here111')
 
   const response = await fetch(`${process.env.REWRITE_HOST}${req.nextUrl.pathname}`, {
     method: req.method,
-    headers: newRequestHeaders
+
   })
+
+  response.headers.set('set-cookie','anuj is here')
+
   console.log("response header--->")
 
   response.headers.forEach((value, key) => {
-    responseHeader += [key, `${value.replaceAll('.countryroad.com.au', '.vercel.app').replaceAll('www.countryroad.com.au', 'next-js-rewrites-anuj.vercel.app')}`]
+    responseHeader += `${key}: ${value}\n`
   })
 
   console.log('responseHeader', responseHeader)
+  
 
 
-  // responseHeader.replaceAll('.countryroad.com.au', '.vercel.app')
-  // responseHeader.replaceAll('www.countryroad.com.au', 'next-js-rewrites-anuj.vercel.app')
-
-  console.log('responseHeaderModified', responseHeader)
 
   if (!!response.headers.get('content-type') && !['html', 'css', 'javascript'].some((type) => response.headers.get('content-type').includes(type))) {
 
